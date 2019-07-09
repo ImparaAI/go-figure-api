995cf5d6ce99763dccf2e4cfc9d0bd991a4238de
Sergio Somma <sdsomma@gmail.com>
2019-07-08 23:22:45 -0700

    Adding cors allow all

--- app/app.go
+++ app/app.go
@@ -1,12 +1,18 @@
 package app
 
 import (
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/echo/v4"
 	"api/app/submission"
 )
 
 func New() (*echo.Echo) {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	  AllowOrigins: []string{"*"},
+	  AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 
 	registerServices(e)
 
