02ce566fa67c986c8463a063a35d4daf30c247cf
Jan Hartigan <janhartigan13@gmail.com>
2019-07-17 22:43:05 -0700

    more input validation tests

--- app/drawing/middleware/drawing_input_is_valid.go
+++ app/drawing/middleware/drawing_input_is_valid.go
@@ -21,8 +21,30 @@ func DrawingInputIsValid(next echo.HandlerFunc) echo.HandlerFunc {
 			return echo.NewHTTPError(http.StatusBadRequest, "There needs to be at least 1 point.")
 		}
 
+		if input.Points[0].Time != 0 {
+			return echo.NewHTTPError(http.StatusBadRequest, "The first point's time must be zero.")
+		}
+
+		if pointsAreSequential(input.Points) == false {
+			return echo.NewHTTPError(http.StatusBadRequest, "Each point's time should be equal to or greater than the previous point.")
+		}
+
 		c.Set("input", input)
 
 		return next(c)
 	}
+}
+
+func pointsAreSequential(points []types.OriginalPoint) bool {
+	var lastPoint types.OriginalPoint
+
+	for i := 0; i < len(points); i++ {
+		if (i != 0) && (points[i].Time < lastPoint.Time) {
+			return false
+		}
+
+		lastPoint = points[i]
+	}
+
+	return true
 }
--- app/drawing/test/submit_test.go
+++ app/drawing/test/submit_test.go
@@ -28,3 +28,27 @@ func TestEmptyPointsField(t *testing.T) {
 	assert.True(t, response.IsBadRequest())
 	assert.Equal(t, `{"message":"There needs to be at least 1 point."}`, response.Body())
 }
+
+func TestInvalidPoints(t *testing.T) {
+	nonInts := []string{`"foo"`, "[]", "4.2"}
+
+	for _, val := range nonInts {
+		response := requester.Post("/drawing", `{"points":[` + val + `]}`)
+		assert.True(t, response.IsBadRequest())
+		assert.Equal(t, `{"message":"The request is not properly formatted."}`, response.Body())
+	}
+}
+
+func TestFirstPointTimeNonZero(t *testing.T) {
+	json := `{"points":[{"x": 4, "y": 5, "time": 0.5}, {"x": 5, "y": 1, "time": 1}]}`
+	response := requester.Post("/drawing", json)
+	assert.True(t, response.IsBadRequest())
+	assert.Equal(t, `{"message":"The first point's time must be zero."}`, response.Body())
+}
+
+func TestNonSequentialTimes(t *testing.T) {
+	json := `{"points":[{"x": 4, "y": 5, "time": 0}, {"x": 5, "y": 1, "time": 0}, {"x": 2, "y": 3, "time": 1.5}, {"x": 6, "y": 3, "time": 1.1}]}`
+	response := requester.Post("/drawing", json)
+	assert.True(t, response.IsBadRequest())
+	assert.Equal(t, `{"message":"Each point's time should be equal to or greater than the previous point."}`, response.Body())
+}
