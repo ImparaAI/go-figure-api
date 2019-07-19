a305fc2df16e084ccfba7fce60ad15d10b8e4bee
Jan Hartigan <janhartigan13@gmail.com>
2019-07-18 22:05:17 -0700

    small cleanup

--- app/drawing/store/sql_drawing.go
+++ app/drawing/store/sql_drawing.go
@@ -2,7 +2,6 @@ package store
 
 import (
 	"time"
-	//"database/sql"
 	"api/app/formatting"
 )
 
--- app/drawing/types/drawing.go
+++ app/drawing/types/drawing.go
@@ -1,16 +1,15 @@
 package types
 
 import (
-	//"time"
 	"api/app/formatting"
 )
 
 type Drawing struct {
-	Id int `db:"id" json:"id"`
-	Featured bool `db:"featured" json:"featured"`
-	OriginalPoints string `db:"originalPoints" json:"originalPoints"`
-	DrawVectors string `db:"drawVectors" json:"drawVectors"`
-	CalculatedDrawVectorCount int `db:"calculatedDrawVectorCount" json:"calculatedDrawVectorCount"`
-	CreatedAt formatting.JSONTime `db:"createdAt" json:"createdAt"`
-	LastDrawVectorCalculatedAt formatting.JSONTime `db:"lastDrawVectorCalculatedAt" json:"lastDrawVectorCalculatedAt"`
+	Id int `json:"id"`
+	Featured bool `json:"featured"`
+	OriginalPoints string `json:"originalPoints"`
+	DrawVectors string `json:"drawVectors"`
+	CalculatedDrawVectorCount int `json:"calculatedDrawVectorCount"`
+	CreatedAt formatting.JSONTime `json:"createdAt"`
+	LastDrawVectorCalculatedAt formatting.JSONTime `json:"lastDrawVectorCalculatedAt"`
 }
