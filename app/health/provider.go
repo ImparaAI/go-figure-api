cf2dea27bebcb08d32915e9b043480bcfc634a70
Jan Hartigan <janhartigan13@gmail.com>
2019-08-23 17:29:20 -0700

    CI push

---
+++ app/health/provider.go
@@ -0,0 +1,12 @@
+package drawing
+
+import (
+	"net/http"
+	"github.com/labstack/echo/v4"
+)
+
+func Register(e *echo.Echo) {
+	e.GET("health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "OK")
+	})
+}
